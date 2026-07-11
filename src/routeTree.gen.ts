d83eb64e7e397986b1c024f08037fa0751bac833
Robsom <robsom@example.com>
2026-07-11 07:22:51 -0300

    Removendo arquivos antigos

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,7 +8,7 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from './routes/__root'
+import { Route as rootRouteImport } from './routes/root'
 import { Route as AuthRouteImport } from './routes/auth'
 import { Route as AuthenticatedRouteRouteImport } from './routes/_authenticated/route'
 import { Route as IndexRouteImport } from './routes/index'
