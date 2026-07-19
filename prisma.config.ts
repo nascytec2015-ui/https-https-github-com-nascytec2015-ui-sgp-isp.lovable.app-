57fb14194032a3d580119ee2892435006399adc7
Robsom <robsom@example.com>
2026-07-19 09:11:12 -0300

    Removendo arquivos antigos

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "src/prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
