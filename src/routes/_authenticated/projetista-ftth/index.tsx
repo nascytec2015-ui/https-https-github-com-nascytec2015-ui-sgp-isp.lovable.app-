f39a9230e049be401104651029911e074cfd9292
Robsom <robsom@example.com>
2026-07-24 07:17:04 -0300

    Removendo arquivos antigos

--- src/routes/_authenticated/projetista-ftth/index.tsx
+++ src/routes/_authenticated/projetista-ftth/index.tsx
@@ -1,9 +1,37 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Card, CardContent } from "@/components/ui/card";
 
-export const Route = createFileRoute('/_authenticated/projetista-ftth/')({
-  component: RouteComponent,
-})
+import Toolbar from "@/components/ftth/Toolbar";
+import Sidebar from "@/components/ftth/Sidebar";
+import Canvas from "@/components/ftth/Canvas";
+import PropertiesPanel from "@/components/ftth/PropertiesPanel";
 
-function RouteComponent() {
-  return <div>Hello "/_authenticated/projetista-ftth/"!</div>
+export const Route = createFileRoute(
+  "/_authenticated/projetista-ftth/"
+)({
+  component: ProjetistaFTTHPage,
+});
+
+function ProjetistaFTTHPage() {
+  return (
+    <div className="flex flex-col h-[calc(100vh-70px)]">
+
+      <Toolbar />
+
+      <div className="flex flex-1 overflow-hidden">
+
+        <Sidebar />
+
+        <Card className="flex-1 rounded-none border-l border-r">
+          <CardContent className="p-0 h-full">
+            <Canvas />
+          </CardContent>
+        </Card>
+
+        <PropertiesPanel />
+
+      </div>
+
+    </div>
+  );
 }
